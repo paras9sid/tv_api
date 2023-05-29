95154aee731dbfdd88d6bbbbcbf59081fbf3eff7
paras9sid <sidharthjainp9@gmail.com>
2023-05-29 17:45:07 +0530

    search button - result in console sync with axios

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const form = document.querySelector("#searchForm");
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
-  //   console.log("Submitted!");
-  console.log(form.elements.query.value);
+  const searchTerm = form.elements.query.value;
+  const res = await axios.get(
+    `https://api.tvmaze.com/search/shows?q=${searchTerm}`
+  );
+  console.log(res.data);
 });
