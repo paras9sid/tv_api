bdd3d2be4c34bb0e249a76690df1c10840f2c7eb
paras9sid <sidharthjainp9@gmail.com>
2023-05-29 18:01:26 +0530

    image medium error avoided with loop

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ form.addEventListener("submit", async (e) => {
   const res = await axios.get(
     `https://api.tvmaze.com/search/shows?q=${searchTerm}`
   );
-  //   console.log(res.data);
-  //   console.log(res.data[0].show.image.medium);
-  const img = document.createElement("img");
-  img.src = res.data[0].show.image.medium;
-  document.body.appendChild(img);
+  //
+  makeImages(res.data);
+  form.elements.query.value = "";
 });
+
+const makeImages = (shows) => {
+  for (let result of shows) {
+    // medium image error avoid of image
+    if (result.show.image) {
+      const img = document.createElement("img");
+      img.src = result.show.image.medium; // error after some iamges - Uncaught (in promise) TypeError: Cannot read properties of null (reading 'medium')
+      document.body.append(img);
+    }
+  }
+};
