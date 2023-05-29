e1cba2e0610c27524a42688898b1fa29d2b9a48d
paras9sid <sidharthjainp9@gmail.com>
2023-05-29 17:54:34 +0530

    image appened in body webpage by button

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ form.addEventListener("submit", async (e) => {
   const res = await axios.get(
     `https://api.tvmaze.com/search/shows?q=${searchTerm}`
   );
-  console.log(res.data);
+  //   console.log(res.data);
+  //   console.log(res.data[0].show.image.medium);
+  const img = document.createElement("img");
+  img.src = res.data[0].show.image.medium;
+  document.body.appendChild(img);
 });
